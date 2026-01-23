52be14ef3391923fd043e2e99efcf70a6325e113
dimadubi <dimadubi666@gmail.com>
2026-01-23 23:07:35 +0300

    l2js3

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -5,5 +5,17 @@
  */
 
 const score = 70 // тестовое значение, можно изменять
+
+if (score <= 49) {
+    grade = "F"
+} else if  (score <= 69) {
+    grade = "D"
+} else if (score <= 79) {
+    grade = "C"
+} else if (score <= 89) {
+    grade = "B"
+} else if (score <= 100) {
+    grade = "A"
+}
 let grade
 // your code
