bb684b5eaceff4b58349836f2c609e852fe6df02
dimadubi <dimadubi666@gmail.com>
2026-02-09 01:24:18 +0300

    l3js2

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,16 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+let num = 0
+
+num = +prompt("Введите число")
+
+function isEven(num) {
+    if (num%2===0){
+        console.log(num + " чётное число")
+    } else {
+        console.log(num + " нечётное число")
+    }
+    return num%2 === 0
+}
+isEven(num);
