3a161ce20df3e18144577f4ccd8462fed9eea83f
dimadubi <dimadubi666@gmail.com>
2026-02-09 01:35:39 +0300

    l3js3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,14 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+let n1 = 0;
+let n2 = 0;
+let n3 = 0;
+
+n1 = +prompt("Введите первое число")
+n2 = +prompt("Введите второе число")
+n3 = +prompt("Введите третье число")
+
+function findLargest(n1, n2, n3) {
+    return Math.max(n1, n2, n3);
+}
+console.log(findLargest(n1, n2, n3) + " наибольшее число");
