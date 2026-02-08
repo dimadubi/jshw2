950d0645d87ee26bffd1d200450d5f14926d997d
dimadubi <dimadubi666@gmail.com>
2026-02-09 01:00:01 +0300

    l3js1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,15 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+let n1 = 10
+let n2 = 20
+
+n1 = prompt("Введите первое число")
+
+n2 = prompt("Введите второе число")
+
+
+function calculateAverage(n1, n2) {
+ return  (n1 + n2) / 2;
+}
+
+console.log(calculateAverage(n1, n2));
