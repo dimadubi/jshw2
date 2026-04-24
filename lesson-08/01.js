c7a5e66602f9494b490b586e21c7dee82085fe96
dimadubi <dimadubi666@gmail.com>
2026-04-24 20:31:11 +0300

    l8js1

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -1,9 +1,21 @@
 /*
-  Нужно написать функцию capitalizeWords, которая принимает строку и делает так, чтобы каждое слово начиналось с заглавной буквы.
+  Нужно написать функцию capitalizeWords, которая принимает строку и делает так, чтобы каждое слово
+  начиналось с заглавной буквы.
   Например, если передать строку "hello world from javascript", функция должна вернуть "Hello World From JavaScript".
 
   🧙‍♂️Эту задачу можно решить при помощь цикла for. Рекомендуем реализовать этот вариант решения, как самый эффективный.
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(str) {
+    let words = str.split(" ")
+
+    for (let i = 0; i < words.length; i++) {
+        words[i] = (words[i][0].toUpperCase() + words[i].slice(1))
+    }
+
+    return words.join(" ")
+}
+
+console.log(capitalizeWords("hello world from javascript"))
+
